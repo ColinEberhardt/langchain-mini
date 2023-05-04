53ce3118e13837988d775d060c3ad603b8ab7e85
Colin Eberhardt <colin.eberhardt@gmail.com>
2023-05-04 19:44:53 +0100

    added ability to merge questions

--- index.mjs
+++ index.mjs
@@ -7,6 +7,9 @@ import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 const rl = readline.createInterface({ input, output });
 
+const promptTemplate = fs.readFileSync("prompt.txt", "utf8");
+const mergeTemplate = fs.readFileSync("merge.txt", "utf8");
+
 const googleSearch = async (question) =>
   await fetch(
     `https://serpapi.com/search?api_key=${process.env.SERPAPI_API_KEY}&q=${question}`
@@ -32,19 +35,8 @@ const tools = {
   },
 };
 
-const question = await rl.question("How can I help? ");
-
-// construct the prompt, using our question
-const promptTemplate = fs.readFileSync("prompt.txt", "utf8");
-let prompt = promptTemplate.replace("${question}", question).replace(
-  "${tools}",
-  Object.keys(tools)
-    .map((toolname) => `${toolname}: ${tools[toolname].description}`)
-    .join("\n")
-);
-
 // use GPT-3.5 to answer the question
-const completePrompt = async (prompt) =>
+const completePrompt = async (prompt) => 
   await fetch("https://api.openai.com/v1/completions", {
     method: "POST",
     headers: {
@@ -61,27 +53,59 @@ const completePrompt = async (prompt) =>
     }),
   })
     .then((res) => res.json())
-    .then((res) => res.choices[0].text);
+    .then((res) => res.choices[0].text)
+    .then((res) => {
+      console.log("\x1b[91m" + prompt + "\x1b[0m");
+      console.log("\x1b[92m" + res + "\x1b[0m");
+      return res;
+    });
 
-let finished = false;
-while (!finished) {
-  const response = await completePrompt(prompt);
+const answerQuestion = async (question) => {
+  // construct the prompt, using our question
+  let prompt = promptTemplate.replace("${question}", question).replace(
+    "${tools}",
+    Object.keys(tools)
+      .map((toolname) => `${toolname}: ${tools[toolname].description}`)
+      .join("\n")
+  );
 
-  // add this to the prompt
-  prompt += response;
+  let finished = false;
+  while (!finished) {
+    const response = await completePrompt(prompt);
 
-  // does the response have an action?
-  const action = response.match(/Action: (.*)/)?.[1];
-  if (action) {
-    const actionInput = response.match(/Action Input: "?(.*)"?/)?.[1];
+    // add this to the prompt
+    prompt += response;
 
-    // execute the action
-    const result = await tools[action].execute(actionInput);
-    prompt += `Observation: ${result}\n`;
-  } else {
-    finished = true;
+    // does the response have an action?
+    const action = response.match(/Action: (.*)/)?.[1];
+    if (action) {
+      const actionInput = response.match(/Action Input: "?(.*)"?/)?.[1];
+
+      // execute the action
+      const result = await tools[action.trim()].execute(actionInput);
+      prompt += `Observation: ${result}\n`;
+    } else {
+      finished = true;
+      return response.match(/Final Answer: (.*)/)?.[1];
+    }
   }
-  console.log(prompt);
+};
+
+const mergeHistory = async (question, history) => {
+  const prompt = mergeTemplate
+    .replace("${question}", question)
+    .replace("${history}", history);
+  return await completePrompt(prompt);
+};
+
+let history = "";
+while (true) {
+  let question = await rl.question("How can I help? ");
+  if (history.length > 0) {
+    question = await mergeHistory(question, history);
+  }
+  const answer = await answerQuestion(question);
+  console.log(answer);
+  history += `Q:${question}\nA:${answer}\n`;
 }
 
-rl.close();
